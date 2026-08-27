bb9562816681406606fca31737b37713d6d02df4
ork707 <greengo.koyot@gmail.com>
2026-08-27 15:37:59 +0300

    changed

--- menu/models.py
+++ menu/models.py
@@ -3,6 +3,21 @@ from django.db import models
 class Pizza(models.Model):
 
     name = models.CharField(max_length=200)
+    toppings = models.ManyToManyField("Topping", blank=True)
+    description = models.TextField(blank=True)
             
     def __str__(self):
         return self.name
+
+class Topping(models.Model):
+
+    name = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return self.name
+
+class Size(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name    
